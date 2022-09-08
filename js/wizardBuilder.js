5178e7a782cb60f61cfdf904276b5c903318e43f
Nansah1 <nana.ansah@gsa.gov>
2022-09-08 16:17:24 +0800

    menu updated

--- js/wizardBuilder.js
+++ js/wizardBuilder.js
@@ -354,7 +354,11 @@ var WizardBuilder = window.WizardBuilder || {};
                 // If the button indicates a "nextStepId", then the onclick should add the next step and navigate to it
                 buttonElement.onclick = function(_) {
                     // push steps array into cookie when user click button
-                    setCookie("steps", getCookie("steps")+","+button.nextStepId, 365);
+                    console.log(button.nextStepId)
+                    if(button.nextStepId==2)
+                        setCookie("steps", "2", 365);
+                    else
+                        setCookie("steps", getCookie("steps")+","+button.nextStepId, 365);
                     WizardBuilder.addStep(wizardElementId, button.nextStepId);
                     WizardBuilder.goToStep(wizardElementId, button.nextStepId);
                 }

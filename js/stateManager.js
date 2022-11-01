0e5336c949fbd3b6b59f5008e2ea405eca4684df
Johnny Luce <johnnyluce@gmail.com>
2022-10-31 21:34:34 -0500

    various changes
    Added stateManager.js to centralize the cookie functions
    
    Fixed loan discovery discovery list to build only the completed steps
    
    Added new warning to walkthrough.html

---
+++ js/stateManager.js
@@ -0,0 +1,63 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+        setCookie("username", user, 365);
+        }
+    }
+}
+
+function userState() {
+    // Returns a number indicating user's returning state
+    // 0 = new user
+    // 1 = returing need to Continue eligibility check 
+    // 2 = returing need to Continue loan discovery 
+
+    if (getCookie("steps") != "") {
+        console.log("Welcome back");
+        
+        var largest = Math.max.apply(Math, getCookie("steps").split(","));
+
+        if (largest < 35) {
+            console.log("Continue eligibility check");
+            return 1;
+        }
+        else {
+            console.log("Continue loan discovery");
+            return 2;
+        }
+    }
+    else {
+        console.log("Welcome new user");
+        return 0;
+    }
+}
+
+function deleteCookies() {
+    setCookie("steps", '', -1);
+}

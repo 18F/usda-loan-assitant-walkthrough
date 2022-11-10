1179f3d23429a3db268afd9091adc4e02fefa9ef
Chris Coletta <ccoletta@fearless.tech>
2022-11-10 18:43:09 -0500

    checkpoint checkin

--- make_forms_json/make_forms_json.py
+++ make_forms_json/make_forms_json.py
@@ -5,7 +5,10 @@ from pathlib import Path
 import click
 from fuzzywuzzy import process
 from openpyxl import load_workbook
+import pandas as pd
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -27,16 +30,23 @@ def find_matches(field_title, pdf_labels, verbose = False):
     return None
 
 
-def pdf_input_attrs(file_name):
+def Get_PDF_Input_Attrs(file_name):
+    """Requires the web server to be running locally.
+    Install Node.JS, then run `npm install http-server -g`, then cd into top level
+    directory of this repo "usda-loan-assitant-walkthrough" and runn the command
+    `http-server` which will then make the following URL valid:"""
+
     url = "http://127.0.0.1:8080/js/pdfjs/web/viewer.html?file=/forms/"+file_name
-    INPUT_ATTR = ["id", "type","data-name", "aria-label"]
+    # Relevant HTML tag attribute names
+    wanted_attributes = ["id", "type", "data-name", "aria-label"]
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
 
     # On some of the forms, there are no text widgets on page 1
     # which I believe causes the webdriver below to time out.
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # This isn't working right now:
+    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     try:
         # If input PDF document was created/edited using Adobe acrobat
@@ -46,15 +56,23 @@ def pdf_input_attrs(file_name):
         # client, perhaps microsoft word
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.xfaTextfield")))
 
-    elements = driver.find_elements(By.CSS_SELECTOR, 'input, textarea')
-    out = {}
-    print( "In", file_name, "found", len( elements ), "elements:" )
-    for el in elements:
-        key = el.get_attribute('data-name') or el.get_attribute('aria-label')
-        pid = el.get_attribute('id')
-        if (key) and pid.endswith('R'):
-            out[key] = {attr: el.get_attribute(attr) for attr in INPUT_ATTR}
-    return out
+    all_pdf_form_inputs = driver.find_elements( By.CSS_SELECTOR, 'input, textarea' )
+    raw_descrip_to_pid_dict = {}
+    print( "In", file_name, "found", len( pdf_form_inputs ), "elements:" )
+    for input_element in all_pdf_form_inputs:
+        description_text = input_element.get_attribute('data-name') or input_element.get_attribute('aria-label')
+        pid = input_element.get_attribute('id')
+        if description_text and pid.endswith('R'):
+            raw_descrip_to_pid_dict[ description_text ] = \
+                { attr_name: input_element.get_attribute( attr_name ) for attr_name in wanted_attributes }
+
+    def StripStopwords( desc_text : str ) -> str:
+        return " ".join( [ _ for _ in word_tokenize( desc_text ) if _ not in stopwords ] )
+
+    stripped_descrip_to_pid_dict = \
+        { StripStopwords( desc_text ) : pid for desc_text, pid in raw_descrip_to_pd_dict.items() }
+
+    return raw_descrip_to_pid_dict, stripped_descrip_to_pid_dict
 
 def process_forms_spreadsheet(source_filename, specific_form_request, form_count, field_count, update_pids=False, verbose=False):
 
@@ -73,59 +91,99 @@ def process_forms_spreadsheet(source_filename, specific_form_request, form_count
     data = {}
     data["Forms"] = []
 
-    inventory_sheet = wb["Form Inventory"]
-    last_forms_row = len(list(inventory_sheet.rows))
+    #inventory_sheet = wb["Form Inventory"]
+    inventory_sheet_df = pd.read_excel( io=source_filename, sheet_name="Form Inventory" )
+    #last_forms_row = len(list(inventory_sheet.rows))
 
-    parts_sheet = wb["Part Inventory"]
-    last_parts_row = len(list(parts_sheet.rows))
+    #parts_sheet = wb["Part Inventory"]
+    parts_sheet_df = pd.read_excel( io=source_filename, sheet_name="Part Inventory" )
+    #last_parts_row = len(list(parts_sheet.rows))
 
     running_form_count = 0
-    for row in range(2, last_forms_row + 1):
+    # iterate over all forms
+    for form_id, form_file_name in inventory_sheet[ ['id','file_name'] ].values:
+    #for row in range(2, last_forms_row + 1):
         running_form_count += 1
         if running_form_count > form_count:
             break
 
-        parts_list = []
-        form_id = inventory_sheet["B" + str(row)].value
-        if specific_form_request is not None and form_id != specific_form_request:
+        #parts_list = []
+        #form_id = inventory_sheet["B" + str(row)].value
+        if specific_form_request is not None and curr_form_id != specific_form_request:
             continue
-        file_name = inventory_sheet["C" + str(row)].value
-        items_sheet  = wb[form_id]
-        last_items_row = len(list(items_sheet.rows))
+        #file_name = inventory_sheet["C" + str(row)].value
+        #items_sheet  = wb[form_id]
+        items_sheet_df = pd.read_excel( io=source_filename, sheet_name=form_id )
+        #last_items_row = len(list(items_sheet.rows))
 
         if verbose:
             print(f' **  Processing form {form_id}')
 
         if update_pids:
-            form_inputs = pdf_input_attrs(file_name)
+            raw_form_inputs, stripped_form_inputs = Get_PDF_Input_Attrs( file_name )
             pdf_labels = form_inputs.keys()
 
-        for part_row in range(2, last_parts_row + 1):
+        form_parts_df = parts_sheet_df[ parts_sheet_df['form_id'] == form_id ]
+        #for part_row in range(2, last_parts_row + 1):
 
-            part_name = parts_sheet["B"+str(part_row)].value
+        # split out the columns into their own lists
+        for part_name, part_title, part_description in form_parts_df[ [ 'name', 'title', 'description' ] ].values:
 
-            if parts_sheet["A"+str(part_row)].value == form_id:
 
-                if verbose:
-                    print(f'   **  Processing part {part_name}')
+            #part_name = parts_sheet["B"+str(part_row)].value
 
-                part_dict = {
-                    "name": part_name,
-                    "title": parts_sheet["C"+str(part_row)].value,
-                    "description": parts_sheet["D"+str(part_row)].value,
-                }
+            #if parts_sheet["A"+str(part_row)].value == form_id:
 
-                items_list = []
-                running_field_count = 0
-                for item_row in range(2, last_items_row + 1):
+            if verbose:
+                print(f'   **  Processing part {part_name}')
 
-                    if part_name == items_sheet["B"+str(item_row)].value:
+            part_dict = {
+                "name": part_name,
+                #"title": parts_sheet["C"+str(part_row)].value,
+                "title": part_title,
+                #"description": parts_sheet["D"+str(part_row)].value,
+                "description": part_description
+            }
 
-                        running_field_count += 1
-                        if running_field_count > field_count:
-                            break
+            #    #items_list = []
+            running_field_count = 0
+            #    for item_row in range(2, last_items_row + 1):
 
-                        field_name = items_sheet["D"+str(item_row)].value or ''
+            #        if part_name == items_sheet["B"+str(item_row)].value:
+            items_in_this_part_df = items_sheet_df[ items_sheet_df[ 'Part' ] == part_name ]
+
+            #[('A', 'PART NAME'),
+            # ('B', 'Part'),
+            # ('C', 'Field #'),
+            # ('D', 'Field Label'),
+            # ('E', 'Original Field Instructions'),
+            # ('F', 'Field Type'),
+            # ('G', 'Input ID'),
+            # ('H', 'Field Count'),
+            # ('I', 'Validation'),
+            # ('J', 'Length Limit'),
+            # ('K', 'Page #'),
+            # ('L', 'Field Left (px)'),
+            # ('M', 'Field Top (px)'),
+            # ('N', 'Recommended field instructions'),
+            # ('O', 'Recommended Validation Rule'),
+            # ('P', 'Error Message'),
+            # ('Q', 'Data Entry Software Application')]
+
+            wanted_columns = [
+                'Field Label',
+                'Field #',
+                'Input ID',
+                'Recommended field instructions',
+                'Page #',
+                '
+            for field_name,  in items_in_this_part.itertuples():
+
+                running_field_count += 1
+                if running_field_count > field_count:
+                    break
+
+                field_name = getattr( item_row_data, #items_sheet["D"+str(item_row)].value or ''
 
                         if verbose:
                             print(f'     **  Processing item {item_row}: {field_name}')

f15634cf393c58ca209eec403a82cbe84d932d08
Chris Coletta <ccoletta@fearless.tech>
2022-11-14 10:40:02 -0500

    Checkpoint checkin

--- make_forms_json/make_forms_json.py
+++ make_forms_json/make_forms_json.py
@@ -1,36 +1,44 @@
 import json
-from pprint import pprint
+#from pprint import pprint
 from datetime import datetime
 from pathlib import Path
+from math import isnan
 import click
 from fuzzywuzzy import process
 from openpyxl import load_workbook
 import pandas as pd
 
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
+stopwords = set(stopwords.words('english'))
 from nltk.tokenize import word_tokenize
-
-from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-def find_matches(field_title, pdf_labels, verbose = False):
-    retval = process.extractOne(field_title, pdf_labels)
+def find_matches( field_title, pdf_labels, verbose = False ):
+    """Tries to match PDF-embedded form input instructions with
+    LAT-enhanced instructions"""
+    if verbose:
+        print( f'        --  QUERY: "{field_title}"' )
+    retval = process.extractOne( field_title, pdf_labels )
     if retval is not None:
         match, rate = retval
+        if verbose:
+            print( f'        --  closest match ({rate}%): "{match}"')
         if rate >= 75:
-            if verbose:
-                print("        --  NEW match: ", match, rate)
             return match
     return None
 
 
-def Get_PDF_Input_Attrs(file_name):
+def Scrape_PDF_Input_Attrs(file_name):
     """Requires the web server to be running locally.
     Install Node.JS, then run `npm install http-server -g`, then cd into top level
     directory of this repo "usda-loan-assitant-walkthrough" and runn the command
@@ -38,7 +46,6 @@ def Get_PDF_Input_Attrs(file_name):
 
     url = "http://127.0.0.1:8080/js/pdfjs/web/viewer.html?file=/forms/"+file_name
     # Relevant HTML tag attribute names
-    wanted_attributes = ["id", "type", "data-name", "aria-label"]
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
@@ -57,32 +64,53 @@ def Get_PDF_Input_Attrs(file_name):
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.xfaTextfield")))
 
     all_pdf_form_inputs = driver.find_elements( By.CSS_SELECTOR, 'input, textarea' )
-    raw_descrip_to_pid_dict = {}
-    print( "In", file_name, "found", len( pdf_form_inputs ), "elements:" )
+    descrip_to_pid_dict = {}
+
+    print( "In", file_name, "found", len( all_pdf_form_inputs ), "elements:" )
+
     for input_element in all_pdf_form_inputs:
-        description_text = input_element.get_attribute('data-name') or input_element.get_attribute('aria-label')
+
+        data_name_text = input_element.get_attribute('data-name')
+        aria_label_text = input_element.get_attribute('aria-label')
+        raw_description_text = data_name_text or aria_label_text
         pid = input_element.get_attribute('id')
-        if description_text and pid.endswith('R'):
-            raw_descrip_to_pid_dict[ description_text ] = \
-                { attr_name: input_element.get_attribute( attr_name ) for attr_name in wanted_attributes }
 
-    def StripStopwords( desc_text : str ) -> str:
-        return " ".join( [ _ for _ in word_tokenize( desc_text ) if _ not in stopwords ] )
+        if not( raw_description_text and pid.endswith('R') ):
+            continue
 
-    stripped_descrip_to_pid_dict = \
-        { StripStopwords( desc_text ) : pid for desc_text, pid in raw_descrip_to_pd_dict.items() }
+        stripped_description_text = \
+            " ".join( [ _ for _ in word_tokenize( raw_description_text ) if _ not in stopwords ] )
 
-    return raw_descrip_to_pid_dict, stripped_descrip_to_pid_dict
+        descrip_to_pid_dict[ stripped_description_text ] = {
+            "id" : pid,
+            "type" : input_element.get_attribute( 'type' ),
+            "data-name" : data_name_text,
+            "aria-label" : aria_label_text,
+            "raw_description_text" : raw_description_text
+        }
 
-def process_forms_spreadsheet(source_filename, specific_form_request, form_count, field_count, update_pids=False, verbose=False):
+    return descrip_to_pid_dict
+
+def process_forms_spreadsheet(
+        source_filename,
+        specific_form_request,
+        form_count,
+        field_count,
+        update_pids=False,
+        verbose=False
+):
 
     if verbose:
-        print(f"Loading {source_filename}")
-    wb = load_workbook(filename = source_filename)
+        print( f"Loading {source_filename}" )
+
+    #wb = load_workbook( filename = source_filename )
 
     input_excel_path_obj = Path( source_filename )
-    output_excel_filepath = input_excel_path_obj.stem + \
-        "_updated_" + datetime.strftime(datetime.now(), '%Y-%m-%d_%H:%M:%S') + \
+
+    output_excel_filepath = \
+        input_excel_path_obj.stem + \
+        "_updated_" + \
+        datetime.strftime( datetime.now(), '%Y-%m-%d_%H:%M:%S' ) + \
         input_excel_path_obj.suffix
 
     if verbose:
@@ -92,49 +120,74 @@ def process_forms_spreadsheet(source_filename, specific_form_request, form_count
     data["Forms"] = []
 
     #inventory_sheet = wb["Form Inventory"]
-    inventory_sheet_df = pd.read_excel( io=source_filename, sheet_name="Form Inventory" )
+    form_sheet_df = pd.read_excel(
+        io=source_filename,
+        sheet_name="Form Inventory"
+    )
     #last_forms_row = len(list(inventory_sheet.rows))
 
     #parts_sheet = wb["Part Inventory"]
-    parts_sheet_df = pd.read_excel( io=source_filename, sheet_name="Part Inventory" )
+    parts_sheet_df = pd.read_excel(
+        io=source_filename,
+        sheet_name="Part Inventory"
+    )
     #last_parts_row = len(list(parts_sheet.rows))
 
     running_form_count = 0
+
+    pid_list = []
+
     # iterate over all forms
-    for form_id, form_file_name in inventory_sheet[ ['id','file_name'] ].values:
+    wanted_form_cols = [ 'name', 'id', 'file_name', 'url', 'description' ]
+    for (
+        form_name,
+        form_id,
+        form_file_name,
+        form_url,
+        form_description
+    ) in form_sheet_df[ wanted_form_cols ].values:
     #for row in range(2, last_forms_row + 1):
         running_form_count += 1
         if running_form_count > form_count:
             break
 
-        #parts_list = []
         #form_id = inventory_sheet["B" + str(row)].value
-        if specific_form_request is not None and curr_form_id != specific_form_request:
+        if specific_form_request is not None and form_id != specific_form_request:
             continue
         #file_name = inventory_sheet["C" + str(row)].value
         #items_sheet  = wb[form_id]
         items_sheet_df = pd.read_excel( io=source_filename, sheet_name=form_id )
         #last_items_row = len(list(items_sheet.rows))
 
+        items_sheet_df[ 'Field Label' ].apply(
+            lambda desc: " ".join( [ _ for _ in word_tokenize( desc ) if _ not in stopwords ] )
+            )
         if verbose:
+            # indent level = 2
             print(f' **  Processing form {form_id}')
 
         if update_pids:
-            raw_form_inputs, stripped_form_inputs = Get_PDF_Input_Attrs( file_name )
-            pdf_labels = form_inputs.keys()
+            form_inputs = Scrape_PDF_Input_Attrs( form_file_name )
+            new_pdf_input_descriptions = form_inputs.keys()
 
         form_parts_df = parts_sheet_df[ parts_sheet_df['form_id'] == form_id ]
         #for part_row in range(2, last_parts_row + 1):
 
-        # split out the columns into their own lists
-        for part_name, part_title, part_description in form_parts_df[ [ 'name', 'title', 'description' ] ].values:
-
+        parts_list = []
+        wanted_parts_cols = [ 'name', 'title', 'description' ]
+        # Iterate over all the parts in the form
+        for (
+            part_name,
+            part_title,
+            part_description
+        ) in form_parts_df[ wanted_parts_cols ].values:
 
             #part_name = parts_sheet["B"+str(part_row)].value
 
             #if parts_sheet["A"+str(part_row)].value == form_id:
 
             if verbose:
+                # Indent level = 4
                 print(f'   **  Processing part {part_name}')
 
             part_dict = {
@@ -145,12 +198,13 @@ def process_forms_spreadsheet(source_filename, specific_form_request, form_count
                 "description": part_description
             }
 
-            #    #items_list = []
             running_field_count = 0
             #    for item_row in range(2, last_items_row + 1):
 
             #        if part_name == items_sheet["B"+str(item_row)].value:
-            items_in_this_part_df = items_sheet_df[ items_sheet_df[ 'Part' ] == part_name ]
+            items_in_this_part_df = items_sheet_df[
+                items_sheet_df[ 'Part' ] == part_name
+            ]
 
             #[('A', 'PART NAME'),
             # ('B', 'Part'),
@@ -171,62 +225,76 @@ def process_forms_spreadsheet(source_filename, specific_form_request, form_count
             # ('Q', 'Data Entry Software Application')]
 
             wanted_columns = [
-                'Field Label',
-                'Field #',
-                'Input ID',
-                'Recommended field instructions',
-                'Page #',
-                '
-            for field_name,  in items_in_this_part.itertuples():
+                'Field #', # Col C
+                'Field Label', # Col D
+                'Input ID', # Col G
+                'Recommended field instructions', # Col N
+                'Page #', # Col K
+                'Field Left (px)', # Col L
+                'Field Top (px)', # Col M
+            ]
+
+            items_list = []
+            # Iterate over all the input elements in this part:
+            for (
+                field_number,
+                field_name,
+                preexisting_pid,
+                new_instructions_text,
+                page_number,
+                field_left_px,
+                field_top_px
+            ) in items_in_this_part_df[ wanted_columns ].values:
 
                 running_field_count += 1
                 if running_field_count > field_count:
                     break
 
-                field_name = getattr( item_row_data, #items_sheet["D"+str(item_row)].value or ''
+                if verbose:
+                    # Indent level = 6
+                    print(f'     **  Processing form {form_id}, {part_name}, field {field_number}')
 
-                        if verbose:
-                            print(f'     **  Processing item {item_row}: {field_name}')
+                new_pid = ''
+                if update_pids:
+                    match = find_matches(
+                        field_name,
+                        new_pdf_input_descriptions,
+                        verbose
+                    )
+                    if match:
+                        new_pid = form_inputs[ match ][ 'id' ]
 
-                        if update_pids:
-                            match = find_matches(field_name, pdf_labels, verbose)
-                            if match:
-                                if verbose:
-                                    print( "          --  ORIGINAL:", form_inputs[match] )
-                                new_pid = form_inputs[match]['id']
-                                items_sheet["G"+str(item_row)].value = new_pid
-                            else:
-                                new_pid = ''
+                pid_list.append( new_pid )
 
-                        item_id = items_sheet["C"+str(item_row)].value or ""
-                        item_dict = {
-                            "id": str(int(item_id)) if isinstance(item_id, float) and item_id == int(item_id) else str(item_id),
-                            "name": field_name,
-                            "comment": items_sheet["N"+str(item_row)].value or '',
-                            "page": items_sheet["K"+str(item_row)].value or 0,
-                            "left": items_sheet["L"+str(item_row)].value or 0,
-                            "top": items_sheet["M"+str(item_row)].value or 0,
-                            # coletta edit: worksheet not updated until wb.save() is called
-                            # which takes a really long time.
-                            #"pid": items_sheet["G"+str(item_row)].value or ''
-                            "pid": new_pid
-                        }
-                        items_list.append(item_dict)
+                # Field number is a text field anyway
+                item_id = "" if( isinstance( field_number, float) and isnan( field_number )) else str( field_number ),
+                item_dict = {
+                    "id": item_id,
+                    "name": field_name,
+                    "comment": new_instructions_text,
+                    "page": page_number or 0,
+                    "left": field_left_px or 0,
+                    "top": field_top_px or 0,
+                    "pid": new_pid
+                }
+                items_list.append(item_dict)
 
-                part_dict['items'] = items_list
-                parts_list.append(part_dict)
+            # END iterating over items in this part
+            part_dict['items'] = items_list
+            parts_list.append( part_dict )
 
+        # END iterating over all the parts
         data["Forms"].append({
-            "name": inventory_sheet["A" + str(row)].value or '',
-            "id": inventory_sheet["B" + str(row)].value or '',
-            "file_name": file_name or '',
-            "url": inventory_sheet["D" + str(row)].value or '',
-            "description":  inventory_sheet["E" + str(row)].value or '',
+            "name": form_name or "",
+            "id": form_id or "",
+            "file_name": form_file_name or '',
+            "url": form_url or "",
+            "description": form_description or "",
             "parts": parts_list
         })
 
-    wb.save( output_excel_filepath )
-    wb.close()
+    #wb.save( output_excel_filepath )
+    #wb.close()
     return data
 
 @click.command()

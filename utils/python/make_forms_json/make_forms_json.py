b9ed1b9f814e581cf396363e5aa082c7cb8facb0
Adam Gradt <adam.grandt@gsa.gov>
2023-01-04 09:26:41 -0500

    Utilities cleanup

--- utils/python/make_forms_json/make_forms_json.py
+++ utils/python/make_forms_json/make_forms_json.py
@@ -5,14 +5,14 @@ import click
 from fuzzywuzzy import process
 
 
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.common.exceptions import TimeoutException
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
 
 
 from pprint import pprint
